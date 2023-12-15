3e87630c7c22b0bd7f87399317016d16aed0c861
Sihnor <m.rudolph.programming@outlook.com>
2023-12-15 21:27:52 +0100

    Added the Present pngs and Add Loaded Claculator

---
+++ Assets/Marcel/LoadedCalculator.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadedCalculator : MonoBehaviour
+{
+    private GameObject[] LoadedObjects;
+    Vector2 CalculatedSpaceshipDirection;
+    float CalculatedMassLeftSide;
+    float CalculatedMassRightSide;
+    
+    [SerializeField] private float SpaceshipWidth = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.LoadedObjects = GameObject.FindGameObjectsWithTag("Package");
+        this.CalculatedSpaceshipDirection.y = 1.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        CalculateDistribution();
+        CalculateSpaceShipDirection();
+    }
+    
+    private void CalculateDistribution()
+    {
+        foreach (GameObject package in this.LoadedObjects)
+        {
+            Vector2 packagePosition = package.transform.position;
+            Vector2 spaceshipPosition = this.transform.position;
+            
+            Vector2 direction = packagePosition - spaceshipPosition;
+
+            if (direction.x < 0.0f)
+            {
+                Rigidbody2D packageRigidbody = package.GetComponent<Rigidbody2D>();
+                float packageMass = packageRigidbody.mass;
+                float massCenterScale = direction.x / this.SpaceshipWidth;
+                this.CalculatedMassLeftSide += (packageMass * massCenterScale);
+            }
+            else
+            {
+                Rigidbody2D packageRigidbody = package.GetComponent<Rigidbody2D>();
+                float packageMass = packageRigidbody.mass;
+                float massCenterScale = direction.x / this.SpaceshipWidth;
+                this.CalculatedMassRightSide += (packageMass * massCenterScale);
+            }
+        }
+    }
+
+    private void CalculateSpaceShipDirection()
+    {
+        if (this.CalculatedMassLeftSide > this.CalculatedMassRightSide)
+        {
+            float difference = 0.0f;
+            difference = this.CalculatedMassLeftSide / (this.CalculatedMassLeftSide + this.CalculatedMassRightSide);
+            this.CalculatedSpaceshipDirection.x = -difference;                
+        }
+        
+        if (this.CalculatedMassLeftSide < this.CalculatedMassRightSide)
+        {
+            float difference = 0.0f;
+            difference = this.CalculatedMassRightSide / (this.CalculatedMassLeftSide + this.CalculatedMassRightSide);
+            this.CalculatedSpaceshipDirection.x = difference;                
+        }
+    }
+    
+    public Vector2 GetCalculatedSpaceshipDirection()
+    {
+        return this.CalculatedSpaceshipDirection;
+    }
+}
