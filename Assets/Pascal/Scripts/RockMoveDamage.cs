21e5c7e5f4b757e50fa1feee423359f3993ff207
TheLurch27 <info@sae.edu>
2023-12-15 23:16:44 +0100

    Finished rocks and collectables

---
+++ Assets/Pascal/Scripts/RockMoveDamage.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockMoveDamage : MonoBehaviour
+{
+    [SerializeField] private string shipTag;
+    [SerializeField] private float directDamageAmount;
+
+    [SerializeField] private Vector2 moveDirection;
+    [SerializeField] private float moveSpeed;
+
+    private Rigidbody2D rigitbody;
+
+    private void Start()
+    {
+        rigitbody = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //if (other.gameObject.CompareTag(shipTag))
+        //{
+        //    HealthSystem effected = other.gameObject.GetComponent<HealthSystem>();
+        //    effected.DealDamage(directDamageAmount);
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        rigitbody.AddForce(moveDirection.normalized * moveSpeed);
+    }
+}
