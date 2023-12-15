2f375b76ac98b844e46d02048ef39eab59f5cc80
TheLurch27 <info@sae.edu>
2023-12-16 00:23:36 +0100

    finished pool spawner

---
+++ Assets/Pascal/Scripts/ObsticalPoolManager.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsticalPoolManager : MonoBehaviour
+{
+    public static ObsticalPoolManager Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance is not null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    System.Random rnd = new System.Random();
+
+    [SerializeField] private List<GameObject> ufos = new List<GameObject>();
+    private int indexPointerUfos = 0;
+    [SerializeField] private List<GameObject> rocks = new List<GameObject>();
+    private int indexPointerRocks = 0;
+    [SerializeField] private List<GameObject> powerups = new List<GameObject>();
+    private int indexPointerPowerups = 0;
+    [SerializeField] private Vector2 hiddenPoolPoint;
+    [SerializeField] private GameObject plane;
+    [SerializeField] private int spawnXRange;
+    [SerializeField] private int spawnYOffset;
+
+    private int frameCounterUfos;
+    private int framesUntillSpawnUfos;
+
+    private int frameCounterRocks;
+    private int framesUntillSpawnRocks;
+
+    private int frameCounterPowerups;
+    private int framesUntillSpawnPowerups;
+
+    private void Start()
+    {
+        foreach (GameObject rock in rocks)
+        {
+            MoveToHiddenPoint(rock);
+        }
+        foreach (GameObject ufo in ufos)
+        {
+            MoveToHiddenPoint(ufo);
+        }
+        foreach (GameObject powerup in powerups)
+        {
+            MoveToHiddenPoint(powerup);
+        }
+        framesUntillSpawnUfos = rnd.Next(2 * 5, 5 * 5);
+        framesUntillSpawnRocks = rnd.Next(2 * 5, 5 * 5);
+        framesUntillSpawnPowerups = rnd.Next(2 * 5, 5 * 5);
+    }
+
+    private void FixedUpdate()
+    {
+        #region Ufos
+        if (frameCounterUfos < framesUntillSpawnUfos)
+        {
+            frameCounterUfos++;
+        }
+        else
+        {
+            frameCounterUfos = 0;
+            framesUntillSpawnUfos = rnd.Next(2 * 5, 5 * 5);
+
+            MoveToSpawnPoint(ufos[indexPointerUfos]);
+            indexPointerUfos++;
+        }
+        #endregion
+
+        #region Rocks
+        if (frameCounterRocks < framesUntillSpawnRocks)
+        {
+            frameCounterRocks++;
+        }
+        else
+        {
+            frameCounterRocks = 0;
+            framesUntillSpawnRocks = rnd.Next(2 * 5, 5 * 5);
+
+            MoveToSpawnPoint(rocks[indexPointerRocks]);
+            indexPointerRocks++;
+        }
+        #endregion
+
+        #region Powerups
+        if (frameCounterPowerups < framesUntillSpawnPowerups)
+        {
+            frameCounterPowerups++;
+        }
+        else
+        {
+            frameCounterPowerups = 0;
+            framesUntillSpawnPowerups = rnd.Next(2 * 5, 5 * 5);
+
+            MoveToSpawnPoint(powerups[indexPointerPowerups]);
+            indexPointerPowerups++;
+        }
+        #endregion
+    }
+
+    public void MoveToHiddenPoint(GameObject _gameobj)
+    {
+        _gameobj.transform.position = hiddenPoolPoint;
+    }
+    public void MoveToSpawnPoint(GameObject _gameobj)
+    {
+        Vector2 planePos = plane.transform.position;
+
+        float randomXPos = rnd.Next((int)(planePos.x - spawnXRange), (int)(planePos.x + spawnXRange));
+
+        Vector2 spawnPoint = new Vector2(randomXPos, planePos.y + spawnYOffset);
+
+        _gameobj.transform.position = spawnPoint;
+    }
+}
